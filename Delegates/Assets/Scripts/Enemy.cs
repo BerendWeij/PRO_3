2515cd038f093015e7f25aae7788f6cddf26c359
Berend Weij <info@berendweij.nl>
2016-03-22 09:45:18 +0100

    added callback example for loading a texture (simple util class)

---
+++ Delegates/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+	void Start () {
+		GetTexture();
+	}
+
+	void GetTexture(){
+		TextureLoader textureLoader = new TextureLoader();
+		textureLoader.Load("http://www.studiozoetekauw.nl/wp-content/uploads/2014/09/liveStudio.jpg",
+		                   TextureLoaded,
+		                   this);
+	}
+
+	void TextureLoaded(Texture texture){
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = texture;
+	}
+
+}
