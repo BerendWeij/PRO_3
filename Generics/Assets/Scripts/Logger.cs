19e96431746e54619b393f57433bf35c70352972
Berend Weij <info@berendweij.nl>
2017-06-19 10:57:49 +0200

    adds pdf for C# Actions and Delegate callbacks

--- Generics/Assets/Scripts/Logger.cs
+++ Generics/Assets/Scripts/Logger.cs
@@ -11,7 +11,8 @@ public class Logger : MonoBehaviour {
 		// 1) probeer verschillende waardes en Typen uit met GetParameterType. Dit mogen ook eigen Class & struct types zijn
 
 		// 2) heb je redelijk wat geprobeerd? Ga dan kijken of je mijn RandomValue method aan kunt roepen. Sla de return value op in een variabele en Debug.Log() deze variabele
-
+		int speed = RandomValue<int> (1, 5);
+		string student = RandomValue<string> ("Yosha", "Jochem");
 	}
 
 	string GetParameterType<T> (T value) {
