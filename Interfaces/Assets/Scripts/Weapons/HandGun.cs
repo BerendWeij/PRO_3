cfe1ce008a12b13edef87a20e07dcd3123ccae9d
Berend Weij <info@berendweij.nl>
2018-12-03 15:32:57 +0100

    updates to new unity

--- Interfaces/Assets/Scripts/Weapons/HandGun.cs
+++ Interfaces/Assets/Scripts/Weapons/HandGun.cs
@@ -1,17 +1,6 @@
 using UnityEngine;
-using System.Collections;
 
 public class HandGun : MonoBehaviour, IWeapon {
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 	
 	public void Shoot() {
 		Debug.Log("shooting HandGun");
--- Interfaces/Assets/Scripts/Weapons/ShotGun.cs
+++ Interfaces/Assets/Scripts/Weapons/ShotGun.cs
@@ -1,17 +1,6 @@
 using UnityEngine;
-using System.Collections;
 
 public class ShotGun : MonoBehaviour, IWeapon {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	public void Shoot() {
 		Debug.Log("shooting ShotGun");
